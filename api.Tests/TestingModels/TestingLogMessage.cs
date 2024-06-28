4bd290e9d0e7970405e77cf9a45ef0d338934ea0
Adam <fiakkasa@live.com>
2024-06-28 16:57:38 +0300

    Test Log Amends

--- api.Tests/TestingModels/TestingLogMessage.cs
+++ api.Tests/TestingModels/TestingLogMessage.cs
@@ -2,4 +2,11 @@ using Microsoft.Extensions.Logging;
 
 namespace api.Tests.TestingModels;
 
-public record TestingLogMessage(LogLevel LogLevel, string? OriginalMessage, KeyValuePair<string, object?>[]? Arguments);
+public record TestingLogMessage(
+    LogLevel LogLevel,
+    string? OriginalMessage,
+    KeyValuePair<string, object?>[]? Arguments,
+    string? ExceptionType = default,
+    string? ExceptionMessage = default,
+    string? ExceptionSource = default
+);
