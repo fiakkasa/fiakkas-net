afcf98ada7c603842f8e62948a7824617bffc7ac
Adam <fiakkasa@live.com>
2024-06-29 12:59:31 +0300

    Amend Health Entities to use implicit conversions

--- api.Application/Models/HealthReportItem.cs
+++ api.Application/Models/HealthReportItem.cs
@@ -2,12 +2,19 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace api.Application.Models;
 
-[ExcludeFromCodeCoverage]
-[AdaptFrom(typeof(HealthReportEntry))]
 public sealed record HealthReportItem
 {
     public string? Description { get; init; }
     public TimeSpan Duration { get; init; }
     public HealthStatus Status { get; init; }
     public IEnumerable<string>? Tags { get; init; }
+
+    public static implicit operator HealthReportItem(HealthReportEntry entry) =>
+        new()
+        {
+            Description = entry.Description,
+            Duration = entry.Duration,
+            Status = entry.Status,
+            Tags = entry.Tags
+        };
 }
--- api.Application/Models/HealthReportSummary.cs
+++ api.Application/Models/HealthReportSummary.cs
@@ -2,10 +2,17 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace api.Application.Models;
 
-[ExcludeFromCodeCoverage]
 public sealed record HealthReportSummary
 {
     public HealthStatus Status { get; init; }
     public TimeSpan TotalDuration { get; init; }
     public IReadOnlyDictionary<string, HealthReportItem>? Entries { get; init; }
+
+     public static implicit operator HealthReportSummary(HealthReport healthReport) =>
+        new()
+        {
+            Status = healthReport.Status,
+            TotalDuration = healthReport.TotalDuration,
+            Entries = healthReport.Entries.ToDictionary(x => x.Key, x => (HealthReportItem)x.Value)
+        };
 }
