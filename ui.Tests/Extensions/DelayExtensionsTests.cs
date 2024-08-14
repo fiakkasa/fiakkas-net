120253dcc35668102dc0aae1aedf41ca5bdc28a5
Adam <fiakkasa@live.com>
2024-08-15 01:04:23 +0300

    Draft

--- ui.Tests/Extensions/DelayExtensionsTests.cs
+++ ui.Tests/Extensions/DelayExtensionsTests.cs
@@ -7,9 +7,9 @@ namespace ui.Tests.Extensions;
 public class DelayExtensionsTests
 {
     [Theory]
-    [InlineData(250, 249, 260, 1000)]
+    [InlineData(250, 249, 275, 1000)]
     [InlineData(-1, 0, 3, 1000)]
-    [InlineData(1000, 249, 260, 250)]
+    [InlineData(1000, 249, 275, 250)]
     public async Task SafeDelayOfInt_Should_Delay_The_Task_By_The_Specified_Delay(
         int delay,
         int startRange,
@@ -33,9 +33,9 @@ public class DelayExtensionsTests
     }
 
     [Theory]
-    [InlineData(250, 249, 260, 1000)]
+    [InlineData(250, 249, 275, 1000)]
     [InlineData(-1, 0, 3, 1000)]
-    [InlineData(1000, 249, 260, 250)]
+    [InlineData(1000, 249, 275, 250)]
     public async Task SafeDelayOfTimeSpan_Should_Delay_The_Task_By_The_Specified_Delay(
         int delay,
         int startRange,
