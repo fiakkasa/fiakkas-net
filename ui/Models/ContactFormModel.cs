d7ff48cef47b7d7a0d959ec23bfec229e03738af
Adam <fiakkasa@live.com>
2025-08-31 21:29:45 +0300

    Package and Tool Updates Custom Verification Code

--- ui/Models/ContactFormModel.cs
+++ ui/Models/ContactFormModel.cs
@@ -3,6 +3,8 @@ namespace ui.Models;
 [ExcludeFromCodeCoverage]
 public record ContactFormModel
 {
+    private int[] _verificationCode = [];
+
     [Required(ErrorMessage = "How about adding your email address!?")]
     [EmailAddress(ErrorMessage = "How about adding your email address!?")]
     public string SenderAddress { get; set; } = string.Empty;
@@ -16,8 +18,18 @@ public record ContactFormModel
     public string Message { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "How about adding the verification code!?")]
-    [Compare(nameof(VerificationCode), ErrorMessage = "That verification code doesn't look quite right...")]
+    [Compare(nameof(VerificationCodeToMatch), ErrorMessage = "That verification code doesn't look quite right...")]
     public string VerificationCodeEntered { get; set; } = string.Empty;
 
-    public string VerificationCode { get; set; } = string.Empty;
+    public string VerificationCodeToMatch { get; private set; } = string.Empty;
+
+    public int[] VerificationCode
+    {
+        get => _verificationCode;
+        set
+        {
+            _verificationCode = value;
+            VerificationCodeToMatch = string.Join(string.Empty, value);
+        }
+    }
 }
